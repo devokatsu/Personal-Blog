ba0a142732de98dfd8b7cb3fdcde5479b33e440b
okatsu <okatsu3d@gmail.com>
2025-05-18 18:14:13 +0330

    Fixed bugs

--- PersonalBlog/settings.py
+++ PersonalBlog/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'home.apps.HomeConfig',
-    'member.apps.MemberConfig',
+    
 ]
 
 MIDDLEWARE = [
--- PersonalBlog/urls.py
+++ PersonalBlog/urls.py
@@ -3,10 +3,9 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from . import settings
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("home.urls")),
-    path('member/', include("django.contrib.auth.urls")),
-    path("member/", include("member.urls")),
+    path("", include("home.urls")),   
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
