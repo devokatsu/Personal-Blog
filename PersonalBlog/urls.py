2a7d388ad9188e8e6425f1455d811313314ba14f
okatsu <okatsu3d@gmail.com>
2025-05-22 16:13:18 +0330

    Fixed bugs

--- PersonalBlog/urls.py
+++ PersonalBlog/urls.py
@@ -6,6 +6,9 @@ from . import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("home.urls")),   
+    path("", include("home.urls")),
+    path('member/', include("django.contrib.auth.urls")),
+    path('member/', include('member.urls')),
+
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
