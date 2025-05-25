9a3c3d7c7b8b7e04ef8d16368a0bde3a50378bda
okatsu <okatsu3d@gmail.com>
2025-05-25 19:48:51 +0330

    Search bar System

--- home/urls.py
+++ home/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import HomePageView, ArticleDetailView, AddArticleView, UpdateArticleView, DeleteArticleView
+from .views import HomePageView, ArticleDetailView, AddArticleView, UpdateArticleView, DeleteArticleView, SearchView
 
 # app_name = 'home'
 
@@ -9,4 +9,5 @@ urlpatterns = [
     path('add/', AddArticleView.as_view(), name='add_article'),
     path('article/edit/<int:pk>', UpdateArticleView.as_view(), name='update_article'),
     path('article/<int:pk>/remove', DeleteArticleView.as_view(), name='delete_article'),
+    path('search/', SearchView.as_view(), name='search'),
 ]
--- home/views.py
+++ home/views.py
@@ -3,6 +3,7 @@ from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from home.models import Article
 from . forms import ArticleForm, ArticleUpdateForm
+from django.db.models import Q
 
 
 # Home Page View Based On classes
@@ -38,3 +39,20 @@ class DeleteArticleView(DeleteView):
     model = Article
     success_url = reverse_lazy('home')
     template_name = 'home/delete_article.html'
+
+
+class SearchView(ListView):
+    model = Article
+    template_name = 'home/search.html'
+
+    def get_queryset(self):
+        query = self.request.GET.get("q")
+        object_list = Article.objects.filter(
+            Q(title__icontains=query) | Q(content__icontains=query)
+        )
+        return object_list
+    
+
+# def search(request):
+#     q = request.Get.get('q')
+#     article = Article.objects.filter(title__icontains=q)
